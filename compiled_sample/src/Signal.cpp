5466fb372d647a37db4b6f17b59edd5c4572f4c4
jettca <jettca1@gmail.com>
2015-03-08 15:43:49 -0400

    reads from file, main loop structure almost in place

--- compiled_sample/src/Signal.cpp
+++ compiled_sample/src/Signal.cpp
@@ -1,33 +1,32 @@
 #include "Signal.h"
 
-#include <soundfile.h>
+#include <sndfile.hh>
 
 using namespace psl;
 
 
-Signal::Signal(std::string filename)
-{
-    int numSamples;
+Signal::Signal(std::string filename) :
+    Signal(fillFromFile(filename))
+{ }
 
-    SoundFileRead sf(filename.c_str());
-    numSamples = sf.getSamples();
-    buffer_ = buffer_t(numSamples, sample_t(2));
-
-    if(sf.getChannels() == 1)
-        for(int i = 0; i < numSamples; i++)
-        {
-            buffer_[i][1] = buffer_[i][0] = sf.getCurrentSampleDouble(0);
-            sf.incrementSample(); 
-        }
-    else
-        for(int i = 0; i < numSamples; i++)
-        {
-            buffer_[i][0] = sf.getCurrentSampleDouble(0);
-            buffer_[i][1] = sf.getCurrentSampleDouble(1);
-            sf.incrementSample(); 
-        }
-}
+Signal::Signal(fill_t fill) :
+    fill_(fill)
+{ }
 
 void Signal::fillBuffer()
 {
+    fill_(buffer_);
+}
+
+fill_t Signal::fillFromFile(std::string filename)
+{
+    SndfileHandle file(filename);
+
+    std::vector<short> fileData(file.channels() * file.frames());
+    file.read(fileData.data(), fileData.size());
+
+    int pos = 0;
+    return [fileData, pos](buffer_t& buffer) {
+        // fill fileData from buffer
+    };
 }
--- compiled_sample/src/Signal.h
+++ compiled_sample/src/Signal.h
@@ -4,20 +4,27 @@
 #include <vector>
 #include <tuple>
 #include <complex>
+#include <functional>
 
 namespace psl
 {
     typedef std::vector<std::complex<double>> sample_t;
     typedef std::vector<sample_t> buffer_t;
+    typedef std::function<void(buffer_t&)> fill_t;
 
     class Signal
     {
     public:
-        Signal(std::string fileName);
+        Signal(std::string filename);
+        Signal(fill_t fill);
 
         void fillBuffer();
 
+        fill_t fill_;
         double sampleRate_;
         buffer_t buffer_;
+
+    private:
+        static fill_t fillFromFile(std::string filename);
     };
 }
