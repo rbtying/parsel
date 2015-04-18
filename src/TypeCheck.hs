1ed9243522081bd2770bda1f81a30f9d7106a544
jettca <jettca1@gmail.com>
2015-04-18 14:17:39 -0400

    added struct data

--- src/TypeCheck.hs
+++ src/TypeCheck.hs
@@ -10,6 +10,7 @@ data Semantics  = Good
                 | Undef Symbol
                 | Multdef Symbol
                 | NoMain
+                | BadStructAttr
                 deriving (Show, Eq)
 
 
@@ -24,7 +25,15 @@ type VarTable = Map.Map Symbol VarData
 
 data VarData = VarData  { varType :: Type
                         , line :: Int
+                        , def :: Def
                         }
 
-typeCheck :: (VarTree, AST) -> Writer [Semantics] AST
-typeCheck (table, ast) = writer (ast, [Good])
+type StructData = Map.Map Type (Map.Map Symbol Type)
+
+typeCheck :: (VarTree, StructData, AST) -> Writer [Semantics] AST
+typeCheck (Empty, sd, ast) = writer (ast, [Good])
+typeCheck ((Node index scope children), sd, ast)
+
+checkExpr :: VarTree -> StructData -> Expr -> Writer [Semantics] AST
+checkExpr tree sd (Literal _ _) = writer (ast, [Good])
+checkExpr tree sd Attr 
