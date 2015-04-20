1c6a9f7577eff6f9a5ee4bb35bc19c007c890af1
Andy <ahh2131@columbia.edu>
2015-04-19 20:30:35 -0400

    checks for main fn and for at least 1 signal return

--- src/SemanticAnalysis.hs
+++ src/SemanticAnalysis.hs
@@ -12,7 +12,18 @@ semAnalysis :: AST -> Writer [Semantics] AST
 semAnalysis ast = defCheck ast >>= typeCheck >>= mainCheck
 
 mainCheck :: AST -> Writer [Semantics] AST
-mainCheck ast = writer (ast, [Good])
+mainCheck ast = if (length (filter findGoodMain ast)) == 1 then writer (ast, [Good]) else writer (ast, [NoMain])
+
+findGoodMain :: TopDef -> Bool
+findGoodMain (Def d) = findGoodMainDef d 
+findGoodMain (Struct (Symbol sym) tsyms) = False
+
+findGoodMainDef :: Def -> Bool
+findGoodMainDef (FuncDef (Symbol sym) tsyms rt expr)
+	| sym == "main" = if length ts > 0 then True else False
+	| otherwise = False
+	 where TupleType ts = rt
+findGoodMainDef (VarDef tsym expr) = False
 
 defCheck :: AST -> Writer [Semantics] (VarTree, StructData, AST)
 defCheck ast = writer ((Empty, Map.empty, ast), [Good]) 
