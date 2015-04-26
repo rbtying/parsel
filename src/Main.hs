d8843761e1ef6a384a3efc1d5ef9936a01617a47
Derek Xingzhou He <derekhe@gmail.com>
2015-04-25 23:33:31 -0400

    Main loop

--- src/Main.hs
+++ src/Main.hs
@@ -17,11 +17,12 @@ main = do
     input <- readFile infile
     let tokens = AlexToken.scanTokens input
         parse = HappyParser.parse tokens
-        (newparse, errors) = runWriter $ semAnalysis parse
-        code =  if null errors
-                then generateCode newparse
-                else "Error: " ++ show errors
-    indent <- return code --readProcess "astyle" [] code
+        code = generateCode parse
+        -- (newparse, errors) = runWriter $ semAnalysis parse
+        -- code =  if null errors
+        --        then generateCode newparse
+        --        else "Error: " ++ show errors
+    indent <- return code--readProcess "astyle" [] code
     writeFile outfile indent
 
 
@@ -29,7 +30,8 @@ main = do
 generateCode :: AST -> [Char]
 generateCode ast = header ++ n:sdecs ++ n:sdefs ++
                     n:topdecs ++ n:maindef ++ code ++ n:mainloop ++ footer ++ "}"
-    where   header = "#include <iostream>\n#include \"outputs.h\"\n"
+    where   header = "#include <iostream>\n#include <functional>\n#include \"outputs.h\"\n\n"
+                  ++ "psl::Chunk<char> chr2Chunk(char chr) { return [=]{return chr;}; }\n\n"
             (sdecs, sdefs, topdecs, code, mainloop) = genTopDefs ast
             -- TODO: make maindef parse arguments into strings
             maindef = "int main(int argc, char **argv) {\n"
