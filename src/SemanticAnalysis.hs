9557c927c93f851c150961ccb807a8963836933d
Andy Hadjigeorgiou <ahh2131@columbia.edu>
2015-04-26 22:16:42 -0400

    struct data added

--- src/SemanticAnalysis.hs
+++ src/SemanticAnalysis.hs
@@ -35,25 +35,38 @@ findGoodMain (Def d) = isGoodMain d
 findGoodMain (Struct _ _) = False
 
 defCheck :: AST -> Writer [Error] (VarScope, StructData, AST)
---defCheck ast = return (Map.empty, Map.empty, ast)
---defCheck ast = return (Map.empty, getStructs ast, ast)
+defCheck ast = return (Map.empty, getStructs ast, ast)
 
---getStructs :: AST -> StructData
---getStructs ast = Map.fromList (map getStruct [1..numberStructs])
- --   where   getStruct n = (getStructType n ast, getSymbols n)
- --           numberStructs = length (structList ast)
+getStructs :: AST -> StructData
+getStructs ast = Map.fromList (map getStruct [1..numberStructs])
+    where   getStruct n = (getStructType n ast, getStructSymbols n ast)
+            numberStructs = length (structList ast)
 
 structList :: AST -> [TopDef]
 structList ast = filter getListStructs ast
+    where   getListStructs (Struct (Symbol _) _) = True
+            getListStructs (Def _) = False
 
-getListStructs :: TopDef -> Bool
-getListStructs (Struct (Symbol sym) ts) = True
-getListStructs (Def _) = False
-
-getStructType :: Int -> AST -> [Char]
+getStructType :: Int -> AST -> Type
 getStructType n ast = structType ((structList ast) !! (n-1))
+    where   structType (Struct (Symbol sym) _) = Type (Symbol sym)
+            structType (Def _) = Type (Symbol "")
 
-structType :: TopDef -> [Char]
-structType (Struct (Symbol sym) _) = sym
-structType (Def _) = "def"
-defCheck ast = return (builtInScope, builtInStructData, ast)
+getStructSymbols :: Int -> AST -> (Map.Map Symbol Type)
+getStructSymbols n ast = Map.fromList (map getSymbolMap [1..numberSymbols])
+    where   getSymbolMap n2 = (getStructSymbol n n2 ast, getSymType n n2 ast)
+            numberSymbols = (symbolLength n ast)
+
+getStructSymbol :: Int -> Int -> AST -> Symbol
+getStructSymbol n n2 ast = getTsym ((structList ast) !! (n-1))
+    where   getTsym (Struct (Symbol _) ts) =  getSymbol (ts !! n2)
+             where   getSymbol (Tsym (Type _) (Symbol s)) = (Symbol s)
+ 
+getSymType :: Int -> Int -> AST -> Type
+getSymType n n2 ast = getTsym ((structList ast) !! (n-1))
+    where   getTsym (Struct (Symbol _) ts) = getTsymType (ts !! n2)
+             where  getTsymType (Tsym  (Type t) (Symbol _)) = (Type t)
+
+symbolLength :: Int -> AST -> Int
+symbolLength n ast = lengthTsym ((structList ast) !! (n-1))
+    where   lengthTsym (Struct (Symbol _) ts) = length ts
