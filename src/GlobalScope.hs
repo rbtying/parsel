d7486d7d77f152df0e4b87df90b2868c4f0d9bb8
jettca <jettca1@gmail.com>
2015-05-10 16:27:42 -0400

    type checking seems to work

--- src/GlobalScope.hs
+++ src/GlobalScope.hs
@@ -3,20 +3,47 @@ module GlobalScope where
 import AST
 
 import qualified Data.Map as Map
-import TypeCheck
+
+type SymbolTable = Map.Map Symbol [Type]
+type StructData = Map.Map Type (Map.Map Symbol Type)
 
 --TODO: finish this!
 builtInScope :: SymbolTable
 builtInScope = Map.fromList table
-    where   table = [ (Symbol "sin", FuncType [f] f)
-                    , (Symbol "cos", FuncType [f] f)
-                    , (Symbol "ft", FuncType [s] fs)
-                    , (Symbol "ift", FuncType [fs] s)
-                    , (Symbol "length", FuncType, [s] f) 
+    where   table = [ (Symbol "sin", [FuncType [f] c])
+                    , (Symbol "cos", [FuncType [f] f])
+                    , (Symbol "ft", [FuncType [s] fs])
+                    , (Symbol "ift", [FuncType [fs] s])
+                    , (Symbol "length", [FuncType [str] f]) 
+                    , (Symbol "intervalMap", [FuncType [t, im, s] s])
+                    , (Symbol "signal", [FuncType [fm] s])
+                    , (Symbol "loadSignal", [FuncType [str] s])
+                    , (Symbol "toComplex", [FuncType [f] c])
+                    , (Symbol "fromComplex", [FuncType [c] f])
+                    , (Symbol "psl::greaterThan", compType)
+                    , (Symbol "psl::multiply", arithmeticType)
                     ]
-            f = Type . Symbol $ "float"
-            s = Type . Symbol $ "signal"
-            fs = Type . Symbol $ "fsignal"
+            f   = Type . Symbol $ "float"
+            z   = Type . Symbol $ "int"
+            h   = Type . Symbol $ "freq"
+            s   = Type . Symbol $ "signal"
+            fs  = Type . Symbol $ "fsignal"
+            t   = Type . Symbol $ "time"
+            i   = Type . Symbol $ "interval"
+            b   = Type . Symbol $ "bool"
+            c   = Type . Symbol $ "complex"
+            str = ListType . Type . Symbol $ "char"
+            im  = FuncType [i] i
+            fm  = FuncType [f] c
+            arithmeticType =    [ FuncType [f, f] f
+                                , FuncType [z, z] z
+                                , FuncType [c, c] c
+                                , FuncType [t, t] t
+                                , FuncType [h, h] h ]
+            compType =  [ FuncType [f, f] b
+                        , FuncType [z, z] b
+                        , FuncType [t, t] b
+                        , FuncType [h, h] b ]
 
 builtInStructData :: StructData
 builtInStructData = Map.fromList [
--- src/SemanticAnalysis.hs
+++ src/SemanticAnalysis.hs
@@ -5,107 +5,40 @@ import TypeCheck
 import GlobalScope
 
 import Control.Monad.Writer
-
+import Data.Maybe
 import qualified Data.Map as Map
 
 
 semAnalysis :: AST -> Writer [Error] AST
-semAnalysis ast = defCheck ast >>= typeCheck >>= mainCheck
+semAnalysis ast = defCheck (ast, getStructs ast) >>= mainCheck
 
 mainCheck :: AST -> Writer [Error] AST
 mainCheck ast
-    | (length (filter findGoodMain ast)) == 1 = return ast
+    | any isJust mains = return ast'
     | otherwise = writer (ast, [NoMain])
-
-findGoodMain :: TopDef -> Bool
-findGoodMain (Def d) = isGoodMain d 
-    where   isGoodMain (FuncDef (Symbol sym) tsyms (TupleType ts) _) =
-                goodArgs && goodts && length ts > 0 && sym == "main"
-                where   goodArgs = length (filter isNotCharList tsyms) == 0
-                        goodts = length (filter isNotSignal ts) == 0
-
-                        isNotCharList (Tsym t _) = t /= cltype
-                            where cltype = ListType . Type . Symbol $ "char"
-
-                        isNotSignal (Type (Symbol s)) = s /= "psl::signal"
-                        isNotSignal _ = True
-
-            isGoodMain (FuncDef _ _ _ _) = False
-            isGoodMain _ = False
-findGoodMain (Struct _ _) = False
-
-defCheck :: AST -> Writer [Error] (VarScope, StructData, AST)
-defCheck ast =
-    do  varScope <- getScopes ast
-        return (varScope, getStructs ast, ast)
-
-getScopes :: AST -> Writer [Error] VarScope
-getScopes ast =
-    do  let topScope = Node builtInScope Empty
-        scopes <- mapM (getTopDefScope topScope) ast
-        return $ Map.unions scopes
-        
-
-getTopDefScope :: ScopeTree -> TopDef -> Writer [Error] VarScope
-getTopDefScope parent (Def def) = getDefScope parent def
-defTopDefScope _ _ = return Map.empty
-
-getDefScope :: ScopeTree -> Def -> Writer [Error] VarScope
-getDefScope parent (FuncDef sym tsyms rt expr) =
-    do  let scope = Node table parent
-            table = Map.insert sym ft $ toScopeTable tsyms
-            ft = FuncType (map (\(Tsym t _) -> t) tsyms) rt
-        scope <- getExprScope scope expr
-        return scope 
-
-getExprScope :: ScopeTree -> Expr -> Writer [Error] VarScope
-getExprScope scope (Var sym ind)
-    | searchForDef scope sym = return table
-    | otherwise = writer (table, [Undef sym])
-    where   table = Map.singleton ind scope
+    where   mains = map toGoodMain ast
+            ast' = map fromM $ zip mains ast
+            
+            fromM (Just td', td) = td'
+            fromM (Nothing, td) = td
 
 
-getExprScope parent (LetExp defs expr) = 
-    do  scopes <- mapM (getExprScope scope) exprs 
-        return $ Map.unions scopes
-        where   scope = Node table parent
-                table = Map.fromList $ map toPair defs
-                toPair (VarDef (Tsym t sym) _) = (sym, t)
-                toPair (FuncDef sym tsyms rt _) = (sym, ft)
-                    where ft = FuncType (map (\(Tsym t _) -> t) tsyms) rt
-                exprs = expr:(map getExpr defs)
-                getExpr (VarDef _ e) = e
-                getExpr (FuncDef _ _ _ e) = e
-getExprScope parent (Lambda tsyms rt expr) = getExprScope scope expr
-    where   scope = Node table parent
-            table = toScopeTable tsyms
-getExprScope scope (Cond expr1 expr2 expr3) =
-    do  let exprs = [expr1, expr2, expr3]
-        scopes <- mapM (getExprScope scope) exprs
-        return $ Map.unions scopes
-getExprScope scope (BinaryOp binop expr1 expr2) =
-    do  let exprs = [expr1, expr2]
-        scopes <- mapM (getExprScope scope) exprs
-        return $ Map.unions scopes
-getExprScope scope (Attr expr _) = getExprScope scope expr
-getExprScope scope (Tuple exprs) =
-    do  scopes <- mapM (getExprScope scope) exprs
-        return $ Map.unions scopes
-getExprScope scope (List exprs) =
-    do  scopes <- mapM (getExprScope scope) exprs
-        return $ Map.unions scopes
-getExprScope scope (UnaryOp unop expr) = getExprScope scope expr
-getExprScope _ _ = return Map.empty
+toGoodMain :: TopDef -> Maybe TopDef
+toGoodMain (Def (FuncDef (Symbol sym) tsyms (TupleType ts) expr))
+    | isGoodMain = Just $ Def (FuncDef (Symbol sym) tsyms (TupleType ts) expr')
+    | otherwise = Nothing
+    where   expr' = List exprs
+            Tuple exprs = expr
+            
+            isGoodMain  = goodArgs && goodts && length ts > 0 && sym == "main"
+            goodArgs = all isCharList tsyms
+            goodts = all isSignal ts
 
-searchForDef :: ScopeTree -> Symbol -> Bool
-searchForDef (Node scope parent) sym
-    | Map.member sym scope = True
-    | otherwise = searchForDef parent sym
-searchForDef Empty _ = False
-
-toScopeTable :: [Tsym] -> SymbolTable
-toScopeTable = Map.fromList . map (\(Tsym t s) -> (s, t))
-
+            isCharList (Tsym t _) = t == cltype
+                where cltype = ListType . Type . Symbol $ "char"
+            isSignal t = t == sigtype
+                where sigtype = Type . Symbol $ "signal"
+toGoodMain _ = Nothing
 
 getStructs :: AST -> StructData
 getStructs ast = Map.fromList (map getStruct [1..numberStructs])
